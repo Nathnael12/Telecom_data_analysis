695b8284b040875b3c4aedb187bc78a0651a72e7
natty <natnaelmasresha@gmail.com>
2022-08-24 23:34:02 +0300

    Distribution of the average throughput per handset type

--- scripts/utils.py
+++ scripts/utils.py
@@ -28,7 +28,7 @@ class Utils:
             print(f"Saving failed {e}")
     
         # Function to calculate missing values by column
-    def missing_values_table(df):
+    def missing_values_table(self,df):
         # Total missing values
         mis_val = df.isnull().sum()
 
@@ -58,17 +58,17 @@ class Utils:
         # Return the dataframe with missing information
         return mis_val_table_ren_columns
 
-    def format_float(value):
+    def format_float(self,value):
         return f'{value:,.2f}'
 
-    def find_agg(df:pd.DataFrame, agg_column:str, agg_metric:str, col_name:str, top:int, order=False )->pd.DataFrame:
+    def find_agg(self,df:pd.DataFrame, agg_column:str, agg_metric:str, col_name:str, top:int, order=False )->pd.DataFrame:
         
         new_df = df.groupby(agg_column)[agg_column].agg(agg_metric).reset_index(name=col_name).\
                             sort_values(by=col_name, ascending=order)[:top]
         
         return new_df
 
-    def convert_bytes_to_megabytes(df, bytes_data):
+    def convert_bytes_to_megabytes(self,df, bytes_data):
 
         """
             This function takes the dataframe and the column which has the bytes values
@@ -89,20 +89,20 @@ class Utils:
         
         return df[bytes_data]
 
-    def plot_hist(df:pd.DataFrame, column:str, color:str)->None:
-        # plt.figure(figsize=(15, 10))
+    def plot_hist(self,df:pd.DataFrame, column:str, color:str)->None:
+        plt.figure(figsize=(12, 7))
         # fig, ax = plt.subplots(1, figsize=(12, 7))
-        sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+        sns.histplot(data=df, x=column, color=color, kde=True)
         plt.title(f'Distribution of {column}', size=20, fontweight='bold')
         plt.show()
 
-    def plot_count(df:pd.DataFrame, column:str) -> None:
+    def plot_count(self,df:pd.DataFrame, column:str) -> None:
         plt.figure(figsize=(12, 7))
         sns.countplot(data=df, x=column)
         plt.title(f'Distribution of {column}', size=20, fontweight='bold')
         plt.show()
         
-    def plot_bar(df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str)->None:
+    def plot_bar(self,df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str)->None:
         plt.figure(figsize=(12, 7))
         sns.barplot(data = df, x=x_col, y=y_col)
         plt.title(title, size=20)
@@ -112,20 +112,20 @@ class Utils:
         plt.ylabel(ylabel, fontsize=16)
         plt.show()
 
-    def plot_heatmap(df:pd.DataFrame, title:str, cbar=False)->None:
+    def plot_heatmap(self,df:pd.DataFrame, title:str, cbar=False)->None:
         plt.figure(figsize=(12, 7))
         sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
         plt.title(title, size=18, fontweight='bold')
         plt.show()
 
-    def plot_box(df:pd.DataFrame, x_col:str, title:str) -> None:
+    def plot_box(self,df:pd.DataFrame, x_col:str, title:str) -> None:
         plt.figure(figsize=(12, 7))
         sns.boxplot(data = df, x=x_col)
         plt.title(title, size=20)
         plt.xticks(rotation=75, fontsize=14)
         plt.show()
 
-    def plot_box_multi(df:pd.DataFrame, x_col:str, y_col:str, title:str) -> None:
+    def plot_box_multi(self,df:pd.DataFrame, x_col:str, y_col:str, title:str) -> None:
         plt.figure(figsize=(12, 7))
         sns.boxplot(data = df, x=x_col, y=y_col)
         plt.title(title, size=20)
@@ -133,7 +133,7 @@ class Utils:
         plt.yticks( fontsize=14)
         plt.show()
 
-    def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+    def plot_scatter(self,df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
         plt.figure(figsize=(12, 7))
         sns.scatterplot(data = df, x=x_col, y=y_col, hue=hue, style=style)
         plt.title(title, size=20)
